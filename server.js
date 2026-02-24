821743bb882dc0737f7a91e4de0c6e738f0ee18c
solomon-hackett <solomonhackett2008@gmail.com>
2026-02-24 14:03:55 +0000

    added account and shop routes

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const app = express();
 app.set("view engine", "ejs");
 
 //import routers
-
+const shopRouter = require("./routes/shop");
+const accountRouter = require("./routes/account");
 
 //home page
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 //routes
-
+app.use("/shop", shopRouter);
+app.use("/account", accountRouter);
 
 app.listen(3000);
