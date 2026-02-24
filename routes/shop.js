a215303fb8c2b48f580853814dac66c04e36cce1
solomon-hackett <solomonhackett2008@gmail.com>
2026-02-24 14:41:27 +0000

    created the shop ejs

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express();
 
 //routes
-router.get('/',(req,res)=>{
-    res.send('shop')
-})
+router.get("/", (req, res) => {
+  res.render("shop/shop");
+});
 
 module.exports = router;
